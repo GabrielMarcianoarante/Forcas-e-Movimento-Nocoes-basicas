b9479f6f91ba3af8b556566c0fcbc0a4e55aa09a
zkkas <gabrielpcgamer23@gmail.com>
2026-04-07 22:52:00 -0300

    melhoria do codigo para salvar 15 imagems para cada teste

--- main.py
+++ main.py
@@ -2,41 +2,35 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
-# dados de x e y para o gráfico
-x = [1,2,3,4,5]
-y = [2,5,7,8,11]
-# função do grafico
-def plot_graph(x, y,):
-    #variáveis para o título e os rótulos dos eixos
-    title = "Gráfico de Dispersão"
-    xlabel = "Eixo X"
-    ylabel = "Eixo Y"
-    # função da reta
-    z = np.polyfit(x,y,1)
+for i in range(15):
 
-    a = z[0]
-    b = z[1]
+    print(f"\n Experimento {i+1}")
 
-    #grafico de dispersão
-    plt.scatter(x, y, label ="Dados Experimentais")
+    #inserir os dados de entrada
+    x = np.array(list(map(float, input("Digite os dados de entrada do x(separados por espaço): ").split())))
+    y = np.array(list(map(float, input("Digite os dados de entrada do y(separados por espaço): ").split())))
 
-    #função da reta
+    #regressão linear
+    z = np.polyfit(x, y, 1)
+    a,b = z
+
     p = np.poly1d(z)
 
-    #equação 
-    equacao = (f"y = {a:.2f} x +{b:.2f}")
-
-    #linha de tendência
-    plt.plot(x, p(x), "r--", label = "Linha de Tendência")
-
-    #configurações do gráfico
-    plt.title(title)
-    plt.text(1,9,equacao, fontsize=12, bbox=dict(facecolor='white'))
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.legend()
+    #grafico
+    plt.scatter(x, y)
+    plt.plot(x, p(x), "r--")
+    
+    #equacao da reta
+    equacao = f"y = {a:.2f}x + {b:.2f}"
+    
+    #configuraçao do grafico
+    plt.text(min(x), max(y), equacao, bbox=dict(facecolor='white', alpha=0.5))
+    plt.title(f"Experimento {i+1}")
+    plt.xlabel("x")
+    plt.ylabel("y")
     plt.grid(True)
-    plt.show()
 
-# chamada da função do grafico
-plot_graph(x, y)
+    #salvar imagem
+    plt.savefig(f"Grafico_{i+1}.png")
+    
+    plt.clf() # Limpa a figura para o próximo experimento
